39d5f8c431cb4ae277683f353a8eafc86a9a136e
shirosuzaku <118347073+shirosuzaku@users.noreply.github.com>
2023-06-30 12:54:28 +0300

    Update main.js

--- threeJs/boiler/main.js
+++ threeJs/boiler/main.js
@@ -1,6 +1,6 @@
 import './style.css'
 console.clear();
-console.log("main");
+console.log("main : ",window.location.pathname);
 
 import * as THREE from  "three";
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls'
